fa42684e0f0b1bdc4ae1a042f597392849b03581
AbbeyLeybel <abbey.leybel@gmail.com>
2016-07-02 19:50:46 -0700

    made radio buttons toggle content visibility in mobile view

--- js/brannans.js
+++ js/brannans.js
@@ -4,12 +4,12 @@ if (window.matchMedia("(min-width: 768px)").matches) {
     });
 }
 $('input.btn:radio').on('click', function() {
-   $('input.btn:radio:not(:checked)').parent().removeClass('btn-active');
+    $('input.btn:radio:not(:checked)').parent().removeClass('btn-active');
     $('input.btn:radio:checked').parent().addClass('btn-active');
     var radio_value = $(this).val();
     if (radio_value == 'about') {
         $('article.about').removeClass('hide');
-        $('li.menu-content.about').addClass('visible-xs');
+        $('li.menu-content.about').toggleClass('visible-xs');
         $('article.contact').addClass('hide');
         $('li.menu-content.contact').removeClass('visible-xs');
         $('article.service-area').addClass('hide');
@@ -20,7 +20,7 @@ $('input.btn:radio').on('click', function() {
         $('article.about').addClass('hide');
         $('li.menu-content.about').removeClass('visible-xs');
         $('article.contact').removeClass('hide');
-        $('li.menu-content.contact').addClass('visible-xs');
+        $('li.menu-content.contact').toggleClass('visible-xs');
         $('article.service-area').addClass('hide');
         $('li.menu-content.service-area').removeClass('visible-xs');
         $('article.brands-appliances').addClass('hide');
@@ -31,7 +31,7 @@ $('input.btn:radio').on('click', function() {
         $('article.contact').addClass('hide');
         $('li.menu-content.contact').removeClass('visible-xs');
         $('article.service-area').removeClass('hide');
-        $('li.menu-content.service-area').addClass('visible-xs');
+        $('li.menu-content.service-area').toggleClass('visible-xs');
         $('article.brands-appliances').addClass('hide');
         $('li.menu-content.brands-appliances').removeClass('visible-xs');
     } else if (radio_value == 'brands-appliances') {
@@ -42,7 +42,7 @@ $('input.btn:radio').on('click', function() {
         $('article.service-area').addClass('hide');
         $('li.menu-content.service-area').removeClass('visible-xs');
         $('article.brands-appliances').removeClass('hide');
-        $('li.menu-content.brands-appliances').addClass('visible-xs');
+        $('li.menu-content.brands-appliances').toggleClass('visible-xs');
     }
 });
 $('.to-contact').on('click', function(e) {
